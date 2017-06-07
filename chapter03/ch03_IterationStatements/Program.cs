b311fcc8ecb78502a80ee1d284cb619178edf1ed
fomenky <fomenky@gmail.com>
2017-06-06 19:37:06 -0500

    ch03_IterationStatements - Section Complete

--- chapter03/ch03_IterationStatements/Program.cs
+++ chapter03/ch03_IterationStatements/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using static System.Console;
 
 namespace ch03_IterationStatements
@@ -16,6 +16,7 @@ namespace ch03_IterationStatements
           }
 
           // do statements
+          /*
           string password = string.Empty;
           do
           {
@@ -23,7 +24,17 @@ namespace ch03_IterationStatements
             password = ReadLine();
           } while (password != "secret");
           WriteLine("Correct!");
+          */
+          // for statements
+          for (int y = 1; y <= 10; y++) {
+            Console.WriteLine(y);
+          }
 
+          // foreach statement
+          string[] names = { "Adam", "Dan", "Ac"};
+          foreach (string name in names) {
+            Console.WriteLine($"{name} has {name.Length} characters");
+          }
         }
     }
 }
