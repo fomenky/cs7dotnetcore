50fedd3e283038d0a40e01b8ec6a00255cebd85f
fomenky <fomenky@gmail.com>
2017-06-09 02:11:06 -0500

    ch04_Assemblies Lesson Completed

---
+++ chapter04/ch04_Assemblies/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ch04_Assemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XDocument();
+            int age = 32;
+            
+        }
+    }
+}
