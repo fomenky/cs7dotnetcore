2572a77f4c0678f8c3ac3cc6d3aaf09595a3487f
fomenky <fomenky@gmail.com>
2017-06-12 19:26:33 -0500

    ch04_Lists - Section Complete

---
+++ chapter04/ch04_Lists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+
+namespace ch04_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new List<string>();
+            cities.Add("London");
+            cities.Add("Paris");
+            cities.Add("Milan");
+            WriteLine("Initial List");
+            foreach (string city in cities)
+            {
+                WriteLine($"  {city}");
+            }
+            WriteLine($"The first city is {cities[0]}.");  
+            WriteLine($"The last city is {cities[cities.Count - 1]}.");
+            
+            cities.Insert(0, "Sydney");
+            WriteLine("After inserting Sydney at index 0"); 
+            foreach (string city in cities) 
+            { 
+             WriteLine($"  {city}"); 
+            } 
+        }
+    }
+}
